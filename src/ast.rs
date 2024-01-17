c796d55bbd133a72f0c081908060143e5fcf3d64
Wojciech Wiśniewski <wojciech@funsafe.xyz>
2024-01-17 23:05:33 +0100

    Working for loops

---
+++ src/ast.rs
@@ -0,0 +1,133 @@
+pub struct Program {
+    function: Box<FunctionDefinition>,
+}
+
+pub trait Evaluate{
+    fn evaluate(&self) -> i64;}
+
+#[derive(Debug)]
+pub struct FunctionDefinition {
+    pub return_type: TypeSpecifier,
+    pub identifier: Identifier,
+    pub arguments: Vec<VariableDeclaration>,
+    pub statement: CompoundStatement,
+}
+impl FunctionDefinition {
+    pub fn new(return_type: TypeSpecifier, identifier: Identifier, arguments: Vec<VariableDeclaration>, statement: CompoundStatement) -> Self {
+        Self {
+            return_type,
+            identifier,
+            arguments,
+            statement,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BlockItem {
+    Statement(Statement),
+    Declaration(VariableDeclaration),
+}
+
+#[derive(Debug)]
+pub enum Declaration {
+    VariableDeclaration(TypeSpecifier, Identifier),
+    VariableDeclarationAssignment(TypeSpecifier, Identifier, Box<Expression>),
+}
+
+#[derive(Debug)]
+pub enum If {
+    SingleBranch(Box<Expression>, Box<Statement>), // if (expr) {statement}
+    TwoBranch(Box<Expression>, Box<Statement>, Box<Statement>), // if (expr) {statement0} else {statement1}
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    ReturnExpression(Box<Expression>),
+    Expression(Box<Expression>),
+    Assignment(Identifier, Box<Expression>),
+    If(If),
+    CompoundStatement(CompoundStatement),
+    For(Option<Box<Expression>>, Option<Box<Expression>>, Option<Box<Expression>>, Box<Statement>),
+    ForDecl(VariableDeclaration, Option<Box<Expression>>, Option<Box<Expression>>, Box<Statement>),
+    While(Box<Expression>, Box<Statement>),
+    Do(Box<Statement>, Box<Expression>),
+    Empty,
+}
+
+#[derive(Debug)]
+pub enum CompoundStatement {
+    Empty,
+    StatementList(Vec<BlockItem>),
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    LiteralNum(i64),
+    UnaryOp(UnaryOp, Box<Expression>),
+    Op(Box<Expression>, BinaryOperator, Box<Expression>),
+    Variable(Identifier),
+    Conditional(Box<Expression>, Box<Expression>, Box<Expression>),
+    Assignment(Identifier, Box<Expression>),
+    CompoundAssignment(Identifier, BinaryOperator, Box<Expression>),
+}
+
+// Precedence: https://en.cppreference.com/w/c/language/operator_precedence
+#[derive(Debug)]
+pub enum BinaryOperator {
+    Multiply,
+    Divide,
+    Modulo,
+    Add,
+    Subtract,
+    LogicAnd,
+    LogicOr,
+    LogicEq,
+    LogicNeq,
+    GreaterThen,
+    LessThen,
+    Greq,
+    Leq,
+}
+
+
+#[derive(Debug)]
+pub enum TypeSpecifier {
+    Char,
+    Short,
+    Int,
+    Long,
+    Signed,
+    Unsigned,
+    Float,
+    Double,
+    Void,
+}
+
+#[derive(Debug)]
+pub struct Identifier {
+    pub name: String,
+}
+
+#[derive(Debug)]
+pub struct VariableDeclaration{
+   type_specifier: TypeSpecifier,
+   identifier: Identifier,
+   expression: Option<Box<Expression>>,
+}
+
+impl VariableDeclaration {
+    pub fn new(type_specifier: TypeSpecifier, identifier: Identifier, expression: Option<Box<Expression>>) -> Self {
+        Self { type_specifier, identifier, expression }
+    }
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Minus,
+    BitwiseNegation,
+    LogicalNegation,
+    PlusPlus,
+    MinusMinus,
+}
+
---
+++ src/main.rs
@@ -0,0 +1,119 @@
+use lalrpop_util::lalrpop_mod;
+
+use crate::calculator1::ExpressionParser;
+use crate::calculator1::FunctionDefinitionParser;
+use crate::calculator1::IdentifierParser;
+use crate::calculator1::IfParser;
+use crate::calculator1::StatementParser;
+mod ast;
+
+lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+                               //
+#[test]
+fn calculator1() {
+    assert!(calculator1::ExpressionParser::new()
+        .parse("1 * 2 + 3*2 - 5")
+        .is_ok());
+    assert!(calculator1::ExpressionParser::new()
+        .parse("1 * 2 + 3*2 - 5 -")
+        .is_err());
+
+//   assert!(calculator1::ParameterListParser::new()
+//       .parse("int argc, char argv, long ptr")
+//       .is_ok());
+    assert!(calculator1::StatementParser::new()
+        .parse("abc = 1;")
+        .is_ok());
+    //   assert!(calculator1::StatementParser::new()
+    //       .parse("int abc;")
+    //       .is_ok());
+
+    assert!(calculator1::StatementParser::new()
+        .parse("abc = 1+2+3;")
+        .is_ok());
+
+    assert!(calculator1::IdentifierParser::new().parse("abcd").is_ok());
+
+    assert!(calculator1::IdentifierParser::new().parse("x").is_ok());
+
+    //   assert!(calculator1::StatementListParser::new()
+    //       .parse("int x ; abc = 1; xyz = 2;")
+    //       .is_ok());
+    //   assert!(calculator1::CompoundStatementParser::new()
+    //       .parse("{ int x ; abc = 1; xyz = 2; }")
+    //       .is_ok());
+    //
+    //   assert!(calculator1::FunctionDefinitionParser::new()
+    //       .parse("int main(int argc, int argv){ int x =1; int y; abc = 1; xyz = 2; return 1+2+y; }")
+    //       .is_ok());
+
+    //   assert!(calculator1::IfParser::new()
+    //       .parse("if (1 + 2 == 4) { return 1;}")
+    //       .is_ok());
+
+    assert!(calculator1::IfParser::new()
+        .parse("if (1 + 2 == 4) return 1;")
+        .is_ok());
+
+    assert!(calculator1::IfParser::new()
+        .parse("if (1 + 2 == 4) return 1; else return 2;")
+        .is_ok());
+
+    assert!(calculator1::ExpressionParser::new()
+        .parse("1 == 1 ? x : y")
+        .is_ok());
+
+    assert!(calculator1::StatementParser::new()
+        .parse("{{{{{}}}}}")
+        .is_ok());
+
+    assert!(calculator1::StatementParser::new()
+        .parse("{i = 1;{{{y=2;{}}}x = 5;}}")
+        .is_ok());
+
+    assert!(calculator1::StatementParser::new()
+        .parse("for(; i < 10;++i){return 1;}")
+        .is_ok());
+
+    //    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    //    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    //    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    //    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
+
+fn main() {
+    // println!(
+    //     "Parsing literal expression: {:#?}",
+    //     calculator1::ExpressionParser::new().parse("1")
+    // );
+    // println!(
+    //     "Parsing literal hex expression: {:#?}",
+    //     calculator1::ExpressionParser::new().parse("0xbadbeef")
+    // );
+    // println!(
+    //     "Parsing literal bin expression: {:#?}",
+    //     calculator1::ExpressionParser::new().parse("0b1111")
+    // );
+    // println!(
+    //     "Parsing statement: {:#?}",
+    //     calculator1::StatementParser::new().parse("return 1;")
+    // );
+    // println!(
+    //     "Parsing identifier: {:#?}",
+    //     calculator1::IdentifierParser::new().parse("Bad_apples123abc")
+    // );
+    println!(
+        "Parsing function: {:#?}",
+        calculator1::StatementParser::new().parse("for(int i = 1; i < 10;++i){return 1;}")
+    );
+    // println!(
+    //     "Parsing expression: {:#?}",
+    //     calculator1::ExpressionParser::new().parse("1 == 2 + 5 && 2 != 5*2")
+    // );
+
+    // check_parser_value("int main(int argc, char argv) { return 1; }");
+
+    // println!("{:#?}", calculator1::StatementListParser::new()
+    //     .parse("int xyz ; abc = 1; xyz = 2;")
+    //     );
+}
