31c8a98bea37606b651059e5d4c03d7e155dda2c
Wojciech Wiśniewski <wojciech@funsafe.xyz>
2024-01-18 13:19:37 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bad C parser
+A C parser based on [lalrpop](https://github.com/lalrpop/lalrpop/tree/master)
+## Grammar
+For grammar, go to [c.lalrpop](src/c.lalrpop) file.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use crate::calculator1::ExpressionParser;
-use crate::calculator1::FunctionDefinitionParser;
-use crate::calculator1::IdentifierParser;
-use crate::calculator1::IfParser;
-use crate::calculator1::StatementParser;
+use crate::c::ExpressionParser;
+use crate::c::FunctionDefinitionParser;
+use crate::c::IdentifierParser;
+use crate::c::IfParser;
+use crate::c::StatementParser;
 use lalrpop_util::lalrpop_mod;
 use std::env;
 use std::fs;
@@ -12,120 +12,120 @@ mod ast;
 mod evalue;
 use evalue::*;
 
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+lalrpop_mod!(pub c); // synthesized by LALRPOP
                                //
 #[test]
 fn calculator1() {
-    assert!(calculator1::ExpressionParser::new()
+    assert!(c::ExpressionParser::new()
         .parse("1 * 2 + 3*2 - 5")
         .is_ok());
-    assert!(calculator1::ExpressionParser::new()
+    assert!(c::ExpressionParser::new()
         .parse("1 * 2 + 3*2 - 5 -")
         .is_err());
 
-    assert!(calculator1::ParameterListParser::new()
+    assert!(c::ParameterListParser::new()
         .parse("int argc, char argv, long ptr")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("abc = 1;")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("abc = 1+2+3;")
         .is_ok());
 
-    assert!(calculator1::IdentifierParser::new().parse("abcd").is_ok());
+    assert!(c::IdentifierParser::new().parse("abcd").is_ok());
 
-    assert!(calculator1::IdentifierParser::new().parse("x").is_ok());
+    assert!(c::IdentifierParser::new().parse("x").is_ok());
 
-    assert!(calculator1::CompoundStatementParser::new()
+    assert!(c::CompoundStatementParser::new()
         .parse("{ int x ; abc = 1; xyz = 2; }")
         .is_ok());
 
-    assert!(calculator1::FunctionDefinitionParser::new()
+    assert!(c::FunctionDefinitionParser::new()
         .parse("int main(int argc, int argv){ int x =1; int y; abc = 1; xyz = 2; return 1+2+y; }")
         .is_ok());
 
-    assert!(calculator1::IfParser::new()
+    assert!(c::IfParser::new()
         .parse("if (1 + 2 == 4) { return 1;}")
         .is_ok());
 
-    assert!(calculator1::IfParser::new()
+    assert!(c::IfParser::new()
         .parse("if (1 + 2 == 4) return 1;")
         .is_ok());
 
-    assert!(calculator1::IfParser::new()
+    assert!(c::IfParser::new()
         .parse("if (1 + 2 == 4){ return 1;} else return 2;")
         .is_ok());
 
-    assert!(calculator1::ExpressionParser::new()
+    assert!(c::ExpressionParser::new()
         .parse("1 == 1 ? x : y")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("{{{{{}}}}}")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("{i = 1;{{{y=2;{}}}x = 5;}}")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("for(int i = 1; i < 10;++i){return 1;}")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("for(i = 1; i < 10;++i){return 1;}")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("while (i == 1) {return 1;}")
         .is_ok());
 
-    assert!(calculator1::StatementParser::new()
+    assert!(c::StatementParser::new()
         .parse("do { i+= 1; } while (1);")
         .is_ok());
-    assert!(calculator1::ProgramParser::new()
+    assert!(c::ProgramParser::new()
         .parse("int main() {a;}")
         .is_ok());
 
-    assert!(calculator1::ProgramParser::new()
+    assert!(c::ProgramParser::new()
         .parse("int main() { main(1,2,3,4); }")
         .is_ok());
-    //    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    //    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    //    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    //    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    //    assert!(c::TermParser::new().parse("22").is_ok());
+    //    assert!(c::TermParser::new().parse("(22)").is_ok());
+    //    assert!(c::TermParser::new().parse("((((22))))").is_ok());
+    //    assert!(c::TermParser::new().parse("((22)").is_err());
 }
 
 fn main() {
     // println!(
     //     "Parsing literal expression: {:#?}",
-    //     calculator1::ExpressionParser::new().parse("1")
+    //     c::ExpressionParser::new().parse("1")
     // );
     // println!(
     //     "Parsing literal hex expression: {:#?}",
-    //     calculator1::ExpressionParser::new().parse("0xbadbeef")
+    //     c::ExpressionParser::new().parse("0xbadbeef")
     // );
     // println!(
     //     "Parsing literal bin expression: {:#?}",
-    //     calculator1::ExpressionParser::new().parse("0b1111")
+    //     c::ExpressionParser::new().parse("0b1111")
     // );
     // println!(
     //     "Parsing statement: {:#?}",
-    //     calculator1::StatementParser::new().parse("return 1;")
+    //     c::StatementParser::new().parse("return 1;")
     // );
     // println!(
     //     "Parsing identifier: {:#?}",
-    //     calculator1::IdentifierParser::new().parse("Bad_apples123abc")
+    //     c::IdentifierParser::new().parse("Bad_apples123abc")
     // );
     // println!(
     //     "Parsing function: {:#?}",
-    //     calculator1::ProgramParser::new().parse("void foo() {} int main() { main(1,2,3,4); }")
+    //     c::ProgramParser::new().parse("void foo() {} int main() { main(1,2,3,4); }")
     // );
 
-    // let parsed = calculator1::ProgramParser::new().parse(
+    // let parsed = c::ProgramParser::new().parse(
     //     "
     //         int fib(int n) {
     //             if (n == 0 || n == 1) {
@@ -155,7 +155,7 @@ fn main() {
     let filepath = "/home/wojciech/projects/studia/metody-i-algorytmy-kompilacji/projekt/parser_project/output/example.c";
     let contents = fs::read_to_string(filepath);
     if let Ok(code) = contents {
-        let parsed = calculator1::ProgramParser::new().parse(&code);
+        let parsed = c::ProgramParser::new().parse(&code);
         if let Ok(parsed) = &parsed {
             println!("Parsed: {:#?}", &parsed);
             let asm: String = parsed.compile();
@@ -182,12 +182,12 @@ fn main() {
 
     // println!(
     //     "Parsing expression: {:#?}",
-    //     calculator1::ExpressionParser::new().parse("1 == 2 + 5 && 2 != 5*2")
+    //     c::ExpressionParser::new().parse("1 == 2 + 5 && 2 != 5*2")
     // );
 
     // check_parser_value("int main(int argc, char argv) { return 1; }");
 
-    // println!("{:#?}", calculator1::StatementListParser::new()
+    // println!("{:#?}", c::StatementListParser::new()
     //     .parse("int xyz ; abc = 1; xyz = 2;")
     //     );
 }
